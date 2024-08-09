ad7c03cdb10b97ec7b40287f241d393a8cf3ee2d
ben0tis <benotis.dev@gmail.com>
2024-08-08 20:36:05 -0400

    feat: add menu and action selection
    Add action menu and way to select an action. For now,
    placeholder match/case statements to test. It works!

--- project.py
+++ project.py
@@ -4,11 +4,34 @@ workouts = [{}]
 #probably a good idea to implement JSON file later down the line for persistence
 
 def main():
-    ...
+    #Placeholder match/case for testing
+    choice = get_choice()
+    match choice:
+        case 1:
+            print("Add exercise")
+        case 2:
+            print("Edit exercise")
+        case 3:
+            print("Remove exercise")
+        case 4:
+            print("Add workout")
+        case 5:
+            print("View workouts")
+        case 6:
+            print("Remove workout")
+        case _:
+            print("Invalid choice")
 
 def get_choice():
     #Display list of actions, then ask for a choice from the user
-    ...
+    print("Welcome to CLIWOP - Please choose and action")
+    print("1. Add an exercise")
+    print("2. Edit and exercise")
+    print("3. Remove an exercise")
+    print("4. Add a workout")
+    print("5. View workouts")
+    print("6. Remove a workout")
+    return int(input("Choice: "))
 
 def add_exercise():
     #Add a new exercise to the list, then specify the name and muscle group
